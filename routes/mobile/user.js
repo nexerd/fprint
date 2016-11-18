7b3d8e4463c0d9a6d336b2f239ab416a499aaf24
vitaiy.suhov <vitaliy-suhov@bk.ru>
2016-11-18 11:14:15 +0400

    fixed bug with wrong method name

--- routes/mobile/user.js
+++ routes/mobile/user.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 var auth = require("../../auth/auth");
 
-router.get('/SignIn', auth.MobileSignIn);
+router.post('/SignIn', auth.MobileSignIn);
 
 module.exports = router;
